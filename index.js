e895334bcd7aa0f9eee7a29de855fa2184f3ff43
Jamie Humphries <jamie.humphries@outlook.com>
2026-03-06 13:03:52 +0000

    Set up challenges based on TXT file

--- index.js
+++ index.js
@@ -1,14 +1,117 @@
 import { WebClient } from "@slack/web-api";
 import "dotenv/config";
+import { readFile } from "fs/promises";
 
-const token = process.env.SLACK_TOKEN;
+const { SLACK_TOKEN, CHANNEL_NAME, TEAMS_SPREADSHEET } = process.env;
 
-const client = new WebClient(token);
-const channel = await findChannel(process.env.CHANNEL_NAME);
+const client = new WebClient(SLACK_TOKEN);
+const channel = await findChannel(CHANNEL_NAME);
+
+let challengeNumber = 1;
+
+const submissionsThreadMessage =
+  "*🧵 Submissions 📸*\n" +
+  `Remember to include your <${TEAMS_SPREADSHEET}|team number> in your submission post.`;
+
+const votingThreadMessage =
+  "*🏆 Voting 🗳️*\n\n" +
+  "This challenge has extra points for the best submission.\n" +
+  "React with ➕ to vote for your favourite(s).\n" +
+  "You cannot vote for your own team’s submission.";
 
 async function run() {
-  const thread = await send("Ho, Ho, Ho, World!");
-  await send("How are you?", thread);
+  const challenges = await parseChallenges();
+  for (const section of challenges) {
+    await sendSectionHeader(section);
+    for (const challenge of section.challenges) {
+      const thread = await sendChallenge(challenge);
+      await startChallengeThread(challenge, thread);
+    }
+  }
+}
+
+async function parseChallenges() {
+  const file = await readFile("./challenges.txt");
+  const lines = file
+    .toString()
+    .split("\n")
+    .filter((line) => line !== "");
+
+  const sections = [];
+  let section;
+  for (const line of lines) {
+    const parts = line.split(/\s+\|\s+/g);
+    if (line.startsWith("--")) {
+      section = parseSection(parts);
+      sections.push(section);
+    } else {
+      const challenge = parseChallenge(parts);
+      section.challenges.push(challenge);
+    }
+  }
+
+  return sections;
+}
+
+function parseSection(parts) {
+  const name = parts[0].replaceAll(/--\s+/g, "");
+  const emoji = parts[1];
+  return { name, emoji, challenges: [] };
+}
+
+function parseChallenge(parts) {
+  const prefixText = parts[0];
+  const mainText = parts[1];
+  const [basePoints, perText] = parts[2].split(/\s+\/\s+/g);
+  const maxPoints = parts[3];
+  return {
+    prefixText,
+    mainText,
+    basePoints: parseInt(basePoints),
+    perText,
+    maxPoints: parseInt(maxPoints),
+    hasWinner: maxPoints && !perText,
+  };
+}
+
+async function sendSectionHeader(section) {
+  const { name, emoji } = section;
+  const message =
+    `*--- ${emoji} ${name.toUpperCase()} ${emoji} ---*\n\n` +
+    "Submit a photo or video…";
+  return await send(message);
+}
+
+async function sendChallenge(challenge) {
+  const { prefixText, mainText, hasWinner } = challenge;
+  const challengeText = `${challengeNumber++}. …${prefixText} *${mainText}*${hasWinner ? " 🏆" : ""}`;
+  const pointsText = buildPointsText(challenge);
+  return await send(`${challengeText}\n_(${pointsText})_`);
+}
+
+function buildPointsText(challenge) {
+  const { basePoints, perText, maxPoints } = challenge;
+
+  const basePointsText = formatPoints(basePoints);
+  if (!maxPoints) {
+    return basePointsText;
+  }
+
+  const maxPointsText = formatPoints(maxPoints);
+  return perText
+    ? `${basePointsText} per ${perText}; maximum ${maxPointsText}`
+    : `${maxPointsText} for winner; ${basePointsText} for all other submissions`;
+}
+
+function formatPoints(number) {
+  return `${number} point${number === 1 ? "" : "s"}`;
+}
+
+async function startChallengeThread(challenge, thread) {
+  await send(submissionsThreadMessage, thread);
+  if (challenge.hasWinner) {
+    await send(votingThreadMessage, thread);
+  }
 }
 
 async function send(message, thread = undefined) {
