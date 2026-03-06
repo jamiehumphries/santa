a35a77e18288df94085b5a1f5e04e9f1ebd5275b
Jamie Humphries <jamie.humphries@outlook.com>
2026-03-06 09:50:26 +0000

    Use Slack API to find channel

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Ho ho ho, world!");
+import { WebClient } from "@slack/web-api";
+import "dotenv/config";
+
+const token = process.env.SLACK_TOKEN;
+const channelName = process.env.CHANNEL_NAME;
+
+const client = new WebClient(token);
+
+async function findConversation(name) {
+  let nextCursor = undefined;
+
+  do {
+    const result = await client.conversations.list({
+      limit: 1000,
+      cursor: nextCursor,
+      types: "public_channel,private_channel",
+    });
+
+    const match = result.channels.find((channel) => channel.name === name);
+    if (match) {
+      return match;
+    }
+
+    nextCursor = result.response_metadata.next_cursor;
+  } while (nextCursor);
+
+  throw new Error(`Could not find channel named '${name}'.`);
+}
+
+const channel = await findConversation(channelName);
+console.log(channel);
